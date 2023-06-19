0c4529d7c8a0d94747edf622efca69c90afa95e9
Dokkuhai <yoshidadokkuhai@gmail.com>
2023-06-19 12:09:25 +0700

    Update

--- sourcecode.c
+++ sourcecode.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 100
+int MAX=100;
 
 struct books{
   char code[10];
@@ -10,6 +10,8 @@ struct books{
   float price;
   float value;
 };
+
+
 //Delete \n when using fgets function;
 void delete_newline(char* str) {
     size_t length = strlen(str);
@@ -19,7 +21,9 @@ void delete_newline(char* str) {
 }
 
 
-void addbook(struct books book[],int* k,int n){
+
+
+void addbook(struct books book[],int *k,int n){
   for(int i=0;i<n;i++){
     printf(" Book %d:\n",*k+1);
     printf(" Enter code: ");
@@ -40,6 +44,8 @@ void addbook(struct books book[],int* k,int n){
 
 
 
+//The function will print out console the list menu
+
 void displaymenu(){
   printf("Select options:\n");
   printf("1.Enter the list of books\n");
@@ -50,26 +56,104 @@ void displaymenu(){
   printf("6.Find by code and edit the book\n");
   printf("7.Find by code and delete the book\n");
   printf("0.Exit\n");
-  
-  
-  
 }
 
+//The function will print out console the list of books
 void display_list_book(struct books book[],int k){
 
-  printf("Code\tTitle\tQuantity\tPrice\tValue\n");
-  for(int i=0;i<k;i++){
-    printf("%s\t%s\t%d\t\t%.2f\t%.2f\n", book[i].code, book[i].title, book[i].quantity, book[i].price, book[i].value);
+   printf("\n%-8s  %-20s  %-8s  %-8s  %-8s\n", "Code", "Title", "Quantity", "Price", "Value");
+  for (int i = 0; i < k; i++) {
+    printf("%-8s  %-20s  %-8d  %-8.2f  %-8.2f\n", book[i].code, book[i].title, book[i].quantity, book[i].price, book[i].value);
   }
-  printf("\n");
+  printf("--------------------------------------------------------\n");
   getchar();
 }
  
-void sortofbook(){
+/*void sortofbook(struct books book[],int k){
+  for(int i=0;i<k;i++){
+    for(int j=i+1;j<k;j++){
+      if(strcmp(book[i].code,book[j].code) >= 0){
+        struct books temp = book[i];
+        book[i] = book[j];
+        book[j] = temp;
+      }
+    }
+  }
+}
+*/
+
+
+//This function will sort the k books using Selection sorts
+void sortofbook(struct books book[],int k){
+int indexmin;
+  for(int i=0;i<k;i++){
+    indexmin = i;
+    for(int j=i;j<k;j++){
+      if(strcmp(book[j].code,book[indexmin].code) <= 0) indexmin =j;
+    }
+ struct books temp = book[i];
+        book[i] = book[indexmin];
+        book[indexmin] = temp;
+  }
+}
+
+void load_data_from_file(){
+  FILE *fp = NULL;
+  fp = fopen("database.dat","r");
+  /*while(fscanf(b "",)){
+
+  }*/
+}
+
+void find_max_price(struct books book[] , int k){
 
 }
 
 
+void find_book_and_edit(struct books book[],int k){
+  char find[10];
+  printf("Find what book?(code):");
+  scanf("%s",find);
+  for (int i=0;i<k;i++){
+    if(strcmp(book[i].code,find) == 0){
+      printf("\n%-8s  %-20s  %-8s  %-8s  %-8s\n", "Code", "Title", "Quantity", "Price", "Value");
+      printf("%-8s  %-20s  %-8d  %-8.2f  %-8.2f\n", book[i].code, book[i].title, book[i].quantity, book[i].price, book[i].value);
+      printf(" Book %d:\n",i);
+      printf(" Edit code: ");
+      scanf("%s",book[i].code);
+      printf(" Edit title: ");
+      getchar();
+      fgets(book[i].title,sizeof(book[i].title),stdin);
+      delete_newline(book[i].title);
+      printf(" Edit quantity: ");
+      scanf("%d",&book[i].quantity);
+      printf(" Edit price: ");
+      scanf("%f",&book[i].price);
+
+    } 
+    else{
+      printf("No book found!");
+    } 
+
+  }
+}
+
+void find_book_and_delete(struct books book[], int k){
+  char find[10];
+  char check;
+  printf("Find what book?(code):");
+  scanf("%s",find);
+  for (int i=0;i<k;i++){
+    if(strcmp(book[i].code,find) == 0){
+      printf("\n%-8s  %-20s  %-8s  %-8s  %-8s\n", "Code", "Title", "Quantity", "Price", "Value");
+      printf("%-8s  %-20s  %-8d  %-8.2f  %-8.2f\n", book[i].code, book[i].title, book[i].quantity, book[i].price, book[i].value);
+      printf("---------------------------------------------------------------------------------------------");
+      printf("Do you want to delete this book(y/n)");
+      scanf("%s",&check);
+      }
+  }
+  
+}
 
 int main()
 {
@@ -77,7 +161,6 @@ int main()
   int k=0,n;
   struct books* book;
   
-
   do{
   displaymenu();
   printf("Your selection <0 -> 7>: ");
@@ -93,9 +176,14 @@ int main()
         display_list_book(book,k);
         break;
       case 3:
+        sortofbook(book,k);
+        display_list_book(book,k);
+        break; 
       case 4:
+        
       case 5:
       case 6:
+         find_book_and_edit(book,k);
       case 7:
       case 0:
       default:
